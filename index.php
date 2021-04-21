8f0d945e6f41e94164fd2ccd70e5d9d6bc04d1d5
madk2o <carbonaromichele@live.fr>
2021-04-21 21:41:06 +0200

    1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+         
+        
+        $a = 3;
+
+        $b = 5;
+
+        $c = 7;
+
+        echo "<p>a :$b </p>";
+        echo "<p>b :$c </p>";
+        echo "<p>c :$a </p>";
+
+
+    ?>
+    
+</body>
+</html>
