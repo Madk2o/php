23ff23a4f9b8cb06ce19e579e36bfdb32889fe32
madk2o <carbonaromichele@live.fr>
2021-05-24 17:17:32 +0200

    css

--- README.md
+++
@@ -1,1 +0,0 @@
-# Initiation-PHP-IFAPME
--- index.php
+++ index.php
@@ -1,4 +1,75 @@
-<?php 
-    $titre = "Mon Apprentissage PHP";
-    require "./common/template.php"; 
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bitly</title>
+    <link rel="stylesheet" href="/styles.css">
+    
+</head>
+<body>
+<header>
+        <a href="#">
+            <img class="imghead" src="/src/img/bitly-logo.png" alt="BitlyLogo">
+        </a>
+        <nav>
+            <ul class="listmod">
+            <li><a href="#">Why Bitly?</a></li>
+            <li><a href="#">Solution</a></li>
+            <li><a href="#">features</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Ressource</a></li>
+            </ul>
+        </nav>
+        <nav>
+            <ul class="listmod">
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Sign up</a></li>
+            <li><a href="#">Get a Quote</a></li>
+            </ul>
+        </nav>
+</header>
+<section class="section1">
+    <div class="div1">
+        <h1>Short links, big results</h1>
+        <p>A URL shortener built with powerful tools to help you grow and protect your brand.</p>
+        <a href="#">Get Started for Free</a>
+    </div>
+
+    <img src="/src/img/background-mobile.jpg" alt="imageBackground">
+
+</section>
+
+<section class="sectionform">
+    <form>
+        <input type="text">
+        <input type="submit">
+    </form>
+    <p>By clicking SHORTEN, you are agreeing to Bitly’s <a href="#"> Terms of Service and Policy</a> 
+        
+        
+    </p>
+</section>
+
+<section class="sectionh2">
+    <h2>The most recognized brands in the world love Bitly</h2>
+    <div class="imagesech2">
+        <img src="/src/img/Logo01.svg" alt="Logo">
+        <img src="/src/img/Logo02.svg" alt="Logo">
+        <img src="/src/img/Logo03.svg" alt="Logo">
+        <img src="/src/img/Logo03b.svg" alt="Logo">
+        <img src="/src/img/Logo04.svg" alt="Logo">
+        <img src="/src/img/Logo05.svg" alt="Logo">
+        <img src="/src/img/Logo06.svg" alt="Logo">
+    </div>
+</section>
+
+
+
+<footer>
+    <img src="/src/img/bitly-logo.png" alt="BitlyLogo">
+    <p>2021 Bitly ©</p>
+</footer>
+</body>
+</html>
